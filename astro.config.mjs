6bbe5bf351dca71d223bedb0d1b76450d7b628aa
Jose Cruzado <josecruzado.1206@gmail.com>
2026-03-07 23:36:21 -0500

    feat: Add site URL to Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://josecruzado.github.io',
   vite: {
     plugins: [tailwindcss()]
   },
